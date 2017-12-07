3e7e909f9050732516a730355636f263dde1c9b4
SJTUwxz <343978531@qq.com>
2017-12-07 11:56:15 +0800

    squeeze D5

--- keras_retinanet/models/jh_retina.py
+++ keras_retinanet/models/jh_retina.py
@@ -138,10 +138,11 @@ def __create_pyramid_features(C3, C4, C5, D5, feature_size=256):
     return P3, P4, P5, P6, P7
 
 def __create_resnet_features(C3, C4, C5):
-    D5 = keras.layers.Conv2D(2048, kernel_size=3, strides=1, padding='same', name='D5_conv1')(C5)
-    D5 = keras.layers.Conv2D(2048, kernel_size=3, strides=1, padding='same', name='D5_conv2')(D5)
+    D5_inter = keras.layers.Conv2D(256, kernel_size=3, strides=1, padding='same', name='D5_conv1')(C5)
+    D5_inter = keras.layers.Conv2D(256, kernel_size=3, strides=1, padding='same', name='D5_conv1')(D5_inter)
+    D5 = keras.layers.Conv2D(2048, kernel_size=3, strides=1, padding='same', name='D5_conv2')(D5_inter)
 
-    D6 = keras.layers.Conv2D(1024, kernel_size=3, strides=2, padding='same', name='D6')(D5)
+    D6 = keras.layers.Conv2D(512, kernel_size=3, strides=2, padding='same', name='D6')(D5)
 
     D7 = keras.layers.Conv2D(256, kernel_size=3, strides=2, padding='same', name='D7')(D6)
     D7_pool = keras.layers.GlobalAveragePooling2D(name='D7_pool')(D7)
