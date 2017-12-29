59656db55eeed71098293e67ebdbc02549c1e186
klaus <klaus.cheng@qq.com>
2017-12-29 18:04:57 -0500

    integrate tensorflow record

--- examples/tf_record_finetune.py
+++ examples/tf_record_finetune.py
@@ -18,6 +18,7 @@ import tensorflow as tf
 
 from preprocess.MyImageGenerator import MyImageDataGenerator
 from preprocess.tf_record_db import TfRecordDb
+from preprocess.evaluate_tf_record_callback import EvaluateTfRecordCallback
 
 
 def create_model(
@@ -25,10 +26,7 @@ def create_model(
         cls=3,
         fix_layers=False):
     image = keras.layers.Input((None, None, 3))
-    model = ResNet50RetinaNet(
-        image,
-        num_classes=10,
-        weights='./data/snapshots/001-retinanet/resnet50_05-0.39389.h5')
+    model = ResNet50RetinaNet(image, num_classes=10, weights=weights)
     D7_pool = model.get_layer('D7_pool').output
     Global_cls = keras.layers.Dense(
         cls, activation='softmax', name='global_3cls')(D7_pool)
@@ -43,56 +41,56 @@ def create_model(
     return new_model
 
 
-def train(run_name, train_label_file, val_label_file, batch_size=32):
+def train(run_name,
+          train_record_path,
+          val_record_path,
+          batch_size=32,
+          weights='./data/snapshots/001-retinanet/resnet50_05-0.39389.h5'):
     """train the model"""
-    print('Creating model, this may take a second...')
-    model = create_model()
+    sess = keras.backend.get_session()
+
+    num_train = 103780
+    train_steps = num_train // batch_size
+
+    # build train model
+    model = create_model(fix_layers=False)
+    train_gen = TfRecordDb(None, 'train', train_record_path)
+    images, labels = train_gen.read_record(batch_size=batch_size)
+    model_input = keras.layers.Input(tensor=images)
+    train_model = model(model_input)
+    train_model = keras.models.Model(inputs=model_input, outputs=train_model)
     optimizer = keras.optimizers.sgd(
         lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)
-    model.compile(
-        loss={'global_3cls': 'categorical_crossentropy'},
+    train_model.compile(
+        loss='categorical_crossentropy',
         optimizer=optimizer,
+        target_tensors=[labels],
         metrics=['accuracy'])
-    # print model summary
-    print(model.summary())
 
-    image_data_generator = MyImageDataGenerator()
-    train_gen = image_data_generator.flow_from_label_file(
-        train_label_file,
-        batch_size=batch_size,
-        is_ergodic_files=None,
-        balance=False)
-    train_steps = train_gen.steps_per_epoch()
-    val_gen = image_data_generator.flow_from_label_file(
-        val_label_file,
-        phase='val',
-        batch_size=batch_size,
-        is_ergodic_files=None,
-        balance=False)
-    val_steps = val_gen.steps_per_epoch()
+    # print model summary
+    # print(model.summary())
+    # print(train_model.summary())
 
     #callbacks
+    val_gen = TfRecordDb(None, 'val', val_record_path)
     snapshot_save_directory = './data/snapshots/{}'.format(run_name)
     log_save_directory = './data/logs/{}'.format(run_name)
     os.makedirs(snapshot_save_directory)
     os.makedirs(log_save_directory)
-    model_check_point = keras.callbacks.ModelCheckpoint(
+    evaluate_callback = EvaluateTfRecordCallback(
+        model, val_gen, sess,
         os.path.join(snapshot_save_directory,
                      'snapshot_{epoch:02d}-{val_loss:.5f}--{val_acc:.5f}.h5'),
-        monitor='val_loss',
-        verbose=1,
-        save_best_only=False)
-    csv_logger = keras.callbacks.CSVLogger(
         os.path.join(log_save_directory, 'training_log.csv'))
 
-    #train
-    model.fit_generator(
-        train_gen,
-        steps_per_epoch=train_steps,
-        epochs=50,
-        validation_data=val_gen,
-        validation_steps=val_steps,
-        callbacks=[model_check_point, csv_logger])
+    # Fit the model using data from the TFRecord data tensors.
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess, coord)
+    train_model.fit(
+        epochs=50, steps_per_epoch=train_steps, callbacks=[evaluate_callback])
+    # Clean up the TF session.
+    coord.request_stop()
+    coord.join(threads)
 
 
 if __name__ == '__main__':
@@ -102,16 +100,16 @@ if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
 
     run_time = time.strftime("%Y%m%d-%H%M%S", time.localtime())
-    branch = '002-finetune'
+    branch = 'integrate-tfrecord'
     run_name = '{}/{}'.format(branch, run_time)
     logging.info('run_name: ' + run_name)
 
     batch_size = 32
-    train_label_file = './data/labels/10w_train.txt'
-    val_label_file = './data/labels/10w_val.txt'
+    train_record_path = './data/tf_records/10w_nsp/train.record'
+    val_record_path = './data/tf_records/10w_nsp/val.record'
 
     train(
         batch_size=batch_size,
-        train_label_file=train_label_file,
-        val_label_file=val_label_file,
+        train_record_path=train_record_path,
+        val_record_path=val_record_path,
         run_name=run_name)
---
+++ preprocess/evaluate_tf_record_callback.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import keras
+import numpy as np
+import tensorflow as tf
+from keras.layers import Input
+from keras.models import Model
+from keras.callbacks import Callback
+
+
+class EvaluateTfRecordCallback(Callback):
+    """evaluate model on tf records"""
+
+    def __init__(self,
+                 model,
+                 tf_record_db,
+                 session,
+                 checkout_save_path=None,
+                 log_save_path=None,
+                 batch_size=32):
+        super(EvaluateTfRecordCallback, self).__init__()
+        self.original_model = model
+        self.tf_record_db = tf_record_db
+        self.session = session
+        self.checkout_save_path = checkout_save_path
+        self.log_save_path = log_save_path
+        self.batch_size = batch_size
+
+        # build eval model
+        eval_input = Input(shape=self.tf_record_db.image_shape + (3, ))
+        eval_model = model(eval_input)
+        eval_model = Model(inputs=eval_input, outputs=eval_model)
+        eval_model.compile(
+            optimizer='rmsprop',
+            loss='categorical_crossentropy',
+            metrics=['accuracy'])
+        self.eval_model = eval_model
+
+        # eval x and y
+        self.x = None
+        self.y = None
+
+    def on_train_begin(self, logs={}):
+        #create log_save file
+        self.csv_file = open(self.log_save_path, 'w')
+        self.csv_file.write('epoch,loss,accuracy,val_loss,val_accurary\n')
+        x, y = self.set_xy()
+        self.x = x
+        self.y = y
+
+    def on_epoch_end(self, epoch, logs={}):
+
+        # evaluate
+        result = self.eval_model.evaluate(x=self.x, y=self.y, batch_size=32)
+        # update logs
+        logs.update({'epoch': epoch})
+        logs.update({'val_loss': result[0]})
+        logs.update({'val_acc': result[1]})
+        self.csv_file.write(
+            '{epoch:02d},{loss:.5f},{acc:.5f},{val_loss:.5f},{val_acc:.5f}\n'.
+            format(**logs))
+        print(
+            '\nepoch: {epoch:02d},loss: {loss:.5f},acc: {acc:.5f},val_loss: {val_loss:.5f},val_acc: {val_acc:.5f}\n'.
+            format(**logs))
+        #save checkpoint
+        self.original_model.save_weights(
+            self.checkout_save_path.format(**logs))
+
+    def set_xy(self):
+        """set x and y"""
+        tfrecords_filename = self.tf_record_db.record_save_path
+        prefix = self.tf_record_db.prefix
+        num_classes = self.tf_record_db.num_classes
+        record_iterator = tf.python_io.tf_record_iterator(
+            path=tfrecords_filename)
+        #get data
+        x_key = '{}/image'.format(self.tf_record_db.prefix)
+        y_key = '{}/label'.format(self.tf_record_db.prefix)
+        imgs = []
+        labels = []
+        for string_record in record_iterator:
+            example = tf.train.Example()
+            example.ParseFromString(string_record)
+            img_string = (example.features.feature[x_key].bytes_list.value[0])
+            label = int(example.features.feature[y_key].int64_list.value[0])
+            img_1d = np.fromstring(img_string, dtype=np.float32)
+            img = np.reshape(img_1d, self.tf_record_db.image_shape + (3, ))
+            imgs.append(img)
+            labels.append(label)
+        imgs = np.array(imgs)
+        labels = keras.utils.to_categorical(labels, num_classes=num_classes)
+        return imgs, labels
